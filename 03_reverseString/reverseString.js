4dac17f72fb04ea48026a6e7a45c7ede0eeaa318
TKCoder13 <TKCoding13@gmail.com>
2023-07-25 17:37:14 -0700

    Pass 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    output = "";
+    for (let i = 0; i < input.length; i++) {
+        output += input.charAt(input.length-i-1);
+    }
+    return output;
 };
 
 // Do not edit below this line
