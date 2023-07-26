5b7aa14ca08fa55662789235f65ed3fabb5cfeea
TKCoder13 <TKCoding13@gmail.com>
2023-07-25 17:08:39 -0700

    Pass 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringIn, numOfTimes) {
+    if (numOfTimes < 0) {
+        return "ERROR";
+    }
+    let output = "";
+    for (let i = 0; i < numOfTimes; i++) {
+        output = output + stringIn;
+    }
+    return output;
 };
 
 // Do not edit below this line
