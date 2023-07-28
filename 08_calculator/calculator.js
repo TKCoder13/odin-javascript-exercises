ad30f70ec3b994562b9ef0f27b34369862329714
TKCoder13 <TKCoding13@gmail.com>
2023-07-28 15:49:47 -0700

    Pass 08 - Not perfect, but it works.  Learn to accept and move on :)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(...inputs) {
+  let output = 0;
+	inputs.forEach(function (item) {
+    output += item;
+  });
+  return output;
 };
 
-const subtract = function() {
-	
+const subtract = function(...inputs) {
+	let output = inputs[0] - inputs[1];
+  return output;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfInputs) {
+	let output = 0;
+	for (let i = 0; i < arrayOfInputs.length; i++) {
+    output += arrayOfInputs[i];
+  }
+  return parseInt(output);
 };
 
-const multiply = function() {
-
+const multiply = function(arrayOfInputs) {
+  let output = arrayOfInputs[0];
+	for (let i = 1; i < arrayOfInputs.length; i++) {
+    output *= arrayOfInputs[i];
+  }
+  return parseInt(output);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let output = base;
+  for (let i = 0; i < power - 1; i++) {
+    output *= base;
+  }
+  return output;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let output = 1;
+  for (let i = 1; i <= n; i++) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
