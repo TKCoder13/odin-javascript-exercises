a1e7ad0311ce7ac93e8093468eff7693266c4f80
TKCoder13 <TKCoding13@gmail.com>
2023-07-28 14:47:35 -0700

    Pass 04 - Learn more about arrow functions and inline functions.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,8 @@
-const removeFromArray = function(array, argToRemove) {
-    let output = [];
-    let outputIndex = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] != argToRemove) {
-            output[outputIndex++] = array[i];
-        }
-    }
-    console.log(output);
-    return output;
+const removeFromArray = function(inputArray, ...argsToRemove) {
+    return inputArray.filter(correctInput => !argsToRemove.includes(correctInput));
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
