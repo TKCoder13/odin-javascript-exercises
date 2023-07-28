43bd0ca1198d7f42fd85ab56d0d19bdbdd2f9cb5
TKCoder13 <TKCoding13@gmail.com>
2023-07-28 13:22:09 -0700

    Complete Test 1 - Test 2 needs work, need to learn how to take in unknown amount of inputs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, argToRemove) {
+    let output = [];
+    let outputIndex = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] != argToRemove) {
+            output[outputIndex++] = array[i];
+        }
+    }
+    console.log(output);
+    return output;
 };
 
 // Do not edit below this line
