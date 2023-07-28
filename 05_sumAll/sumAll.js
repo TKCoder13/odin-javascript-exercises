cfaa02ade8c09c06c208ae639e971bf33c59a265
TKCoder13 <TKCoding13@gmail.com>
2023-07-28 15:02:43 -0700

    Pass 05 - There must be a more efficient way of writing these if statements.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(extreme1, extreme2) {
+    let output = 0;
+    if (typeof extreme1 != "number" || typeof extreme2 != "number") return "ERROR";
+    if (extreme1 < 0 || extreme2 < 0) return "ERROR";
+    if (extreme1 < extreme2) {
+        while (extreme1 <= extreme2) {
+            output += extreme1++;
+        }
+    } else if (extreme1 > extreme2) {
+        while (extreme1 >= extreme2) {
+            output += extreme2++;
+        }
+    }
+    
+    return output;
 };
 
 // Do not edit below this line
