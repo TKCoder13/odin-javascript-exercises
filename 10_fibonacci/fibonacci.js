2fcddea327a2bbdfb4dca75f11a79d410b2f7a43
TKCoder13 <TKCoding13@gmail.com>
2023-07-28 20:32:30 -0700

    Pass 10 - Recursion works in JS

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let n_0 = 0;
+    let n_1 = 1;
+    let n_2 = 1;
+    if (n < 0) {
+        return "OOPS";
+    } else if (n == 0) {
+        return n_0;
+    } else if (n == 1) {
+        return n_1;
+    } else if (n == 2) {
+        return n_2;
+    } else if (n > 2) {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
 };
 
 // Do not edit below this line
