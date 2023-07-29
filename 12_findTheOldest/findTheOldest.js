5a50217ffedd52ff71c288ff780ab87af26a98cd
TKCoder13 <TKCoding13@gmail.com>
2023-07-28 23:07:55 -0700

    Pass 12 - Line 8 is a bit weird syntax-wise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestName = "";
+    let oldestAge = 0;
+    let oldestPerson = [];
+    for (let i = 0; i < people.length; i++) {
+        let currentName = people[i].name;
+        let currentAge = 0;
+        if (!people[i].yearOfDeath) {
+            currentDate = new Date().getFullYear();
+            currentAge = currentDate - people[i].yearOfBirth;
+        } else {
+            currentAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+        
+        let currentPerson = people[i];
+        if (i == 0) {
+            oldestName = currentName;
+            oldestAge = currentAge;
+            oldestPerson = currentPerson;
+        } else if (oldestAge < currentAge) {
+            oldestAge = currentAge;
+            oldestName = currentName;
+            oldestPerson = currentPerson;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
