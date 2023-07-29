98fa5c58a00f24483360d0637acb410249ab7e0f
TKCoder13 <TKCoding13@gmail.com>
2023-07-28 20:40:19 -0700

    Pass 11 - Accessing objects in arrays, similar to how Objects work in other languages :

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const bookTitles = [];
+    let bookTitlesIndex = 0;
+    for (let i = 0; i < books.length; i++) {
+        bookTitles[bookTitlesIndex++] = books[i].title;
+    }
+    return bookTitles;
 };
 
 // Do not edit below this line
